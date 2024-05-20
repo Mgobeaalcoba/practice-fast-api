5a794ea5cc09f7aef350ff253a38e68ab42fa3ec
Mariano Daniel Gobea Alcoba <mariano.gobea@mercadolibre.com>
2024-05-20 11:10:48 -0300

    :sparkles: Add Fielf Class for extra validations in models

--- model/items.py
+++ model/items.py
@@ -1,11 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class Item(BaseModel):
     """
-    This is the model for the item. If you receive a item object in the request body, it will be validated
+    This is the model for the item. If you receive a item object in the request body, it will be validated.
+    Using the Field class, you can specify additional validation rules for each field. Directly in the model, you can specify the default value for each field.
     """
     name: str
-    description: str | None = None
-    price: float
+    description: str | None = Field(
+        default=None,
+        title="The description of the item",
+        max_length=300
+    )
+    price: float = Field(
+        gt=0,
+        description="The price must be greater than zero"
+    )
     tax: float | None = None
