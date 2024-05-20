f64c36c244aff64f94b16a935cf30f4e099de5a2
Mariano Daniel Gobea Alcoba <mariano.gobea@mercadolibre.com>
2024-05-20 11:53:40 -0300

    :sparkles: Extra data types

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI, Query, Path, Body
 
+from datetime import datetime, timedelta, time
+
+from uuid import UUID
+
 from typing import Union, Annotated
 
 from model.model_name import ModelName
@@ -382,3 +386,25 @@ async def update_item26(
 ):
     results = {"item_id": item_id, "item": item}
     return results
+
+
+# Example of Extra Data Types of FastApi
+@app.put("/items27/{item_id}")
+async def read_items27(
+    item_id: UUID,
+    start_datetime: Annotated[datetime, Body()],
+    end_datetime: Annotated[datetime, Body()],
+    process_after: Annotated[timedelta, Body()],
+    repeat_at: Annotated[time | None, Body()] = None,
+):
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "process_after": process_after,
+        "repeat_at": repeat_at,
+        "start_process": start_process,
+        "duration": duration,
+    }
